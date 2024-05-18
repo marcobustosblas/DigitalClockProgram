7f310d15c63742ad444b2cc661642c108b149155
Marco <mar.bustosb@duocuc.cl>
2024-05-17 20:18:38 -0400

    it changed logic in hours

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function UpdatedClock(){
     
     let hours = date.getHours();
     const meridium = hours >= 12 ? "PM" : "AM";
-    hours = hours % 12;
+    hours = hours % 12 || 12;
     hours = hours.toString().padStart(2,0);
     const minutes = date.getMinutes().toString().padStart(2,0);
     const seconds = date.getSeconds().toString().padStart(2,0);
@@ -25,12 +25,14 @@ function UpdatedClock(){
     // Get the elements of body
     const body = document.body;
 
-    if(hours <= 12 && meridium === "PM") {
+    // After 12:00 PM (noon)
+    if (hours >= 12 && meridium === "PM") { 
         body.style.backgroundImage = `url('${sunrise}')`;
-    }else if(hours >= 7 && meridium === "PM") {
+    // After 7:00 PM
+    } else if (hours >= 7 && meridium === "PM") { 
         body.style.backgroundImage = `url('${night}')`;
-    }else{
-        body.style.backgroundImage = `url('${day}')`
+    } else {
+        body.style.backgroundImage = `url('${day}')`;
     }
 
 }
